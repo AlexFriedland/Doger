e5a61b4eb0ecb18d84f93054727c7eb1fcce34ff
Alexander Friedland <friedlandaj@gmail.com>
2018-01-12 03:57:25 +0000

    migrations

--- db/migrate/20180112035311_create_dogs.rb
+++ db/migrate/20180112035311_create_dogs.rb
@@ -1,4 +1,7 @@
 class CreateDogs < ActiveRecord::Migration[5.1]
   def change
+    create_table :dogs do |t|
+      t.string :name
+    end
   end
 end
--- db/migrate/20180112035324_create_walks.rb
+++ db/migrate/20180112035324_create_walks.rb
@@ -1,4 +1,12 @@
 class CreateWalks < ActiveRecord::Migration[5.1]
   def change
+    create_table :walks do |t|
+      t.datetime :day
+      t.string :from
+      t.string :to
+      t.decimal :miles
+      t.integer :dog_id
+      t.integer :user_id
+    end
   end
 end
