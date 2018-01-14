e687f90bc931d28d4cca5fed0731c35e71c69a1e
Alexander Friedland <friedlandaj@gmail.com>
2018-01-14 21:55:48 +0000

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,11 @@ h = User.create(username: "harrison", email: "h@harrison.com", password: "h")
 h.dogs << Dog.create(name: "Sophie", user_id: "#{h.id}")
 h.dogs << Dog.create(name: "Mingie", user_id: "#{h.id}")
 
-y = Walk.create(day: "#{Time.now}", from: "house", to: "house", miles: "1.15")
+walk2 = Walk.create(day: "#{Time.now}", from: "house", to: "house", miles: "1.15")
+walk1 = Walk.create(day: "#{Time.now}", from: "x location", to: "y locale", miles: "666")
 
 Dog.all.each do |dog|
-  dog.walks << y
+  dog.walks << [walk1, walk2]
   y.dogs << dog
 end
 
