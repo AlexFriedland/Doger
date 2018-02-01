e87f395e9d6f387d72a3987e82659a68177da856
Alexander Friedland <friedlandaj@gmail.com>
2018-02-01 22:28:09 +0000

    join table for dog_walks, remove columns from walks and dogs

--- db/migrate/20180201044957_has_many_through.rb
+++ db/migrate/20180201044957_has_many_through.rb
@@ -1,30 +1,39 @@
 class HasManyThrough < ActiveRecord::Migration[5.1]
   def change
-    create_join_table :users, :walks do |t|
-      t.integer :user_id
+    create_join_table :dogs, :walks do |t|
+      t.integer :dog_id
       t.integer :walk_id
     end
+
+    #remove walk_id from dogs, has through dog_walks
+    remove_column :dogs, :walk_id
+
+    #remove dog_id from walks, has through dog_walks
+    remove_dolumn :walks, :dog_id
   end
 end
 
-create_table "dogs", force: :cascade do |t|
-  t.string "name"
-  t.integer "walk_id"
-  t.integer "user_id"
-end
 
-#has_many :user_walks
-#has_many :walks, through: :user_walks
-create_table "users", force: :cascade do |t|
-  t.string "username"
-  t.string "email"
-  t.string "password_digest"
-end
+=begin
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "walk_id"
+    t.integer "user_id"
+  end
 
-create_table "walks", force: :cascade do |t|
-  t.datetime "day"
-  t.string "from"
-  t.string "to"
-  t.decimal "miles"
-  t.integer "dog_id"
-end
+  #has_many :user_walks
+  #has_many :walks, through: :user_walks
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+  create_table "walks", force: :cascade do |t|
+    t.datetime "day"
+    t.string "from"
+    t.string "to"
+    t.decimal "miles"
+    t.integer "dog_id"
+  end
+=end
