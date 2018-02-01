92c454b895b19b28eda0b88f2984c72295ec5511
Alexander Friedland <friedlandaj@gmail.com>
2018-02-01 01:39:14 +0000

    walks logic

--- app/controllers/walk_controller.rb
+++ app/controllers/walk_controller.rb
@@ -1,5 +1,39 @@
 class WalksController < ApplicationController
 
-  
+  get "/walks/:id/edit" do
+    if logged_in?
+      @walk = Walk.find_by_id(params[:id])
+      erb :'/walks/edit_walk'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/walks/new" do
+    if logged_in?
+      erb :'/walks/create_walk'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/walks/:id" do
+    if logged_in?
+      @walk = Walk.find_by_id(params[:id])
+      erb :'/walks/show_walk'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/walks" do
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      @dogs = @user.dogs
+      erb :'/walks/walks'
+    else
+      redirect to "/login"
+    end
+  end
 
 end
